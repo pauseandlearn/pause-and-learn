0db0471f6bb20647cea119426e0d6995780b147c
pauseandlearn <k8sstudygroup@gmail.com>
2025-08-09 11:55:58 -0400

    update installation script

--- k8s-cluster-config/on-prem/manual-install/kubeadm-install.sh
+++ k8s-cluster-config/on-prem/manual-install/kubeadm-install.sh
@@ -33,14 +33,16 @@ sudo systemctl enable --now containerd
 sudo containerd config default | sudo tee /etc/containerd/config.toml
 sudo sed -i 's/ SystemdCgroup = false/ SystemdCgroup = true/' /etc/containerd/config.toml
 
-sudo apt-get update
+sudo apt-get update -y
 
 
 sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt update -y
-sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-get install -y kubelet='1.32.5-*' kubeadm='1.32.5-*' kubectl='1.32.5-*'
 sudo apt-mark hold kubelet kubeadm kubectl
+sudo apt update -y
+
